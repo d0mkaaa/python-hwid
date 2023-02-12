5e0443dff146591e11dd3d8bc453b87bd2ee40ac
lmaoleonix <120003853+lmaoleonix@users.noreply.github.com>
2023-02-12 16:00:30 +0200

    Update gethwid.py

--- gethwid.py
+++ gethwid.py
@@ -2,10 +2,6 @@ import hashlib
 import os, platform
 
 def get_hwid():
-    # Get the hardware ID by hashing the combination of certain device properties
-    # You can modify this function to get the HWID in a way that suits your needs
-    # For example, you can use the MAC address, CPU ID, disk volume serial number, etc.
-    # In this example, we use the MAC address, disk volume serial number, and processor ID
     node = os.getenv("computername") + platform.processor() + os.getenv("SystemDrive")
     hwid = hashlib.sha256(node.encode()).hexdigest()
     return hwid
